9bd5de2070246a5e6c6df20385bab29010165d42
Ashen Lalantha <143162341+ashenkv@users.noreply.github.com>
2025-08-16 11:23:04 +0530

    Add files via upload

---
+++ downDocs.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>down docs</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/css/bootstrap.min.css" integrity="sha512-b2QcS5SsA8tZodcDtGRELiGv5SaKSk1vDHDaQRda0htPYWZ6046lr3kJ5bAAQdpV2mmA/4v0wQF9MyU6/pDIAg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.11.2/font/bootstrap-icons.min.css" integrity="sha512-D1liES3uvDpPrgk7vXR/hR/sukGn7EtDWEyvpdLsyalQYq6v6YUsTUJmku7B4rcuQ21rf0UTksw2i/2Pdjbd3g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <!-- <div class="px-3">
+        <img src="resource/sglass.jpg" style="width:10%;">
+    </div> -->
+    <!-- <div>
+        <a href="resource/sglass.jpg" download="img1" class="text-decoration-none fs-5 text-center px-3">download img &nbsp;<i class="bi bi-arrow-bar-down"></i></a>
+    </div> -->
+    <!-- <div>
+        <a href="../resource/sglass.jpg" download="" class="text-decoration-none fs-6 text-center px-3 text-dark"> &nbsp;<i class="fa-solid fa-download"></i></a>
+    </div> -->
+
+    <section class="py-5 px-3">
+        <div class="border rounded p-2 col-10 offset-1 col-lg-4 offset-lg-0 text-center">
+            <h2>Download Company Application</h2>
+            <img src="https://img.freepik.com/free-vector/stack-paper-documents-signature-agreement-with-seal-cartoon-vector-illustration-isolated-white_107791-4239.jpg?w=996&t=st=1708053193~exp=1708053793~hmac=f8c6150cae561a5d8b5dcd8f8b9495a9f861ad39fb1f4c62b531ef06cd6aa74d" style="width:100%;">
+            <a href="resource/pdfs/test.pdf" download="test1"
+             class="btn btn-info text-decoration-none d-grid my-2 p-2 text-center text-light fw-semibold">Download</a>
+             <p class="text-secondary">( This is an application prepared by our organization to obtain new employee details. | මෙය අප ආයතනය විසින් නවක සේවක විස්තර ලබාගැනීම සදහා සකසා ඇති අයදුම්පත්‍රයකි. )</p>
+        </div>
+    </section>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.2/js/bootstrap.bundle.min.js" integrity="sha512-X/YkDZyjTf4wyc2Vy16YGCPHwAY8rZJY+POgokZjQB2mhIRFJCckEGc6YyX9eNsPfn0PzThEuNs+uaomE5CO6A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</body>
+</html>
